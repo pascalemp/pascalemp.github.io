60a3d8b8711f7cb74a3c21a7998f363700e89191
Michele Pascale <michele.pascale111@gmail.com>
2026-06-03 12:18:03 +0100

    Removed "projects" section

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,23 +11,4 @@ const blog = defineCollection({
   })
 });
 
-const projects = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.coerce.date(),
-    description: z.string(),
-    tags: z.array(z.string()).default([]),
-    links: z
-      .array(
-        z.object({
-          label: z.string(),
-          href: z.string().url()
-        })
-      )
-      .default([]),
-    featured: z.boolean().default(false)
-  })
-});
-
-export const collections = { blog, projects };
+export const collections = { blog };
