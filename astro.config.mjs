2ada1845fd81a70225188149071c1448a9ea0db1
Michele Pascale <michele.pascale111@gmail.com>
2026-06-08 11:59:11 +0100

    Replaced KaTeX rendering backend with MathJaX instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import 'katex/dist/katex.min.css';
+import rehypeMathjax from 'rehype-mathjax';
 
 export default defineConfig({
   site: 'https://pascalemp.github.io',
   integrations: [sitemap()],
   markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-    shikiConfig: {
-      theme: 'css-variables',
-      wrap: true
-    }
-  }
+    remarkPlugins: [[remarkMath, { singleDollarTextMath: true }]],
+    rehypePlugins: [rehypeMathjax],
+  },
 });
